c844207151314a74994bf9dae21c36c4bad6970c
kthksgy <1616191+kthksgy@users.noreply.github.com>
2023-08-05 22:00:20 +0900

    テストを追加

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -10,7 +10,9 @@ import {
   NAME,
   VERSION,
   isDefaultProcedureRegistered,
+  isProcedureRegistered,
   registerDefaultProcedure,
+  registerProcedure,
   reset,
 } from './index';
 
@@ -29,7 +31,115 @@ describe('定数定義', function () {
   });
 });
 
+test(`${isDefaultProcedureRegistered.name}`, function () {
+  reset();
+
+  /** 手続き名 */
+  const procedureName = 'procedure';
+  /** 手続き */
+  async function procedure() {
+    return true;
+  }
+
+  expect(isDefaultProcedureRegistered(procedureName)).toBe(false);
+
+  /** 登録を解除する。 */
+  const unregister = registerDefaultProcedure(procedureName, procedure);
+
+  expect(isDefaultProcedureRegistered(procedureName)).toBe(true);
+  expect(isDefaultProcedureRegistered(procedureName, procedure)).toBe(true);
+
+  unregister();
+
+  expect(isDefaultProcedureRegistered(procedureName)).toBe(false);
+});
+
+test(`${isProcedureRegistered.name}`, function () {
+  reset();
+
+  /** 手続き名 */
+  const procedureName = 'procedure';
+  /** 手続き */
+  async function procedure() {
+    return true;
+  }
+
+  expect(isProcedureRegistered(procedureName)).toBe(false);
+
+  /** 登録を解除する。 */
+  const unregister = registerProcedure(procedureName, procedure);
+
+  expect(isProcedureRegistered(procedureName)).toBe(true);
+  expect(isProcedureRegistered(procedureName, procedure)).toBe(true);
+
+  unregister();
+
+  expect(isProcedureRegistered(procedureName)).toBe(false);
+});
+
 describe(`${registerDefaultProcedure.name}`, function () {
+  beforeEach(function () {
+    reset();
+  });
+
+  test('登録と登録解除', function () {
+    /** 手続き名 */
+    const procedureName = 'procedure';
+    /** 手続き */
+    async function procedure() {
+      return true;
+    }
+
+    /** 登録を解除する。 */
+    const unregister = registerDefaultProcedure(procedureName, procedure);
+    expect(isDefaultProcedureRegistered(procedureName, procedure)).toBe(true);
+    expect(isProcedureRegistered(procedureName, procedure)).toBe(true);
+
+    unregister();
+
+    expect(isDefaultProcedureRegistered(procedureName)).toBe(false);
+    expect(isProcedureRegistered(procedureName)).toBe(false);
+  });
+
+  test('上書き', function () {
+    /** 手続き名 */
+    const procedureName = 'procedure';
+    /** 手続き1 */
+    async function procedure1() {
+      return true;
+    }
+    /** 手続き2 */
+    async function procedure2() {
+      return true;
+    }
+
+    /** 登録を解除する。 */
+    const unregister1 = registerDefaultProcedure(procedureName, procedure1);
+    expect(isDefaultProcedureRegistered(procedureName, procedure1)).toBe(true);
+    expect(isDefaultProcedureRegistered(procedureName, procedure2)).toBe(false);
+    expect(isProcedureRegistered(procedureName, procedure1)).toBe(true);
+    expect(isProcedureRegistered(procedureName, procedure2)).toBe(false);
+
+    /** 登録を解除する。 */
+    const unregister2 = registerDefaultProcedure(procedureName, procedure2);
+    expect(isDefaultProcedureRegistered(procedureName, procedure1)).toBe(false);
+    expect(isDefaultProcedureRegistered(procedureName, procedure2)).toBe(true);
+    expect(isProcedureRegistered(procedureName, procedure1)).toBe(false);
+    expect(isProcedureRegistered(procedureName, procedure2)).toBe(true);
+
+    unregister1();
+    expect(isDefaultProcedureRegistered(procedureName, procedure1)).toBe(false);
+    expect(isDefaultProcedureRegistered(procedureName, procedure2)).toBe(true);
+    expect(isProcedureRegistered(procedureName, procedure1)).toBe(false);
+    expect(isProcedureRegistered(procedureName, procedure2)).toBe(true);
+
+    unregister2();
+    expect(isDefaultProcedureRegistered(procedureName)).toBe(false);
+    expect(isProcedureRegistered(procedureName)).toBe(false);
+  });
+});
+
+describe(`${registerProcedure.name}`, function () {
   beforeEach(function () {
     reset();
   });
@@ -38,16 +148,39 @@ describe(`${registerDefaultProcedure.name}`, function () {
     async function procedure() {
       return true;
     }
-    registerDefaultProcedure('procedure', procedure);
-    expect(isDefaultProcedureRegistered('procedure')).toBe(true);
-    expect(isDefaultProcedureRegistered('procedure', procedure)).toBe(true);
+    registerProcedure('procedure', procedure);
+    expect(isProcedureRegistered('procedure')).toBe(true);
+    expect(isProcedureRegistered('procedure', procedure)).toBe(true);
   });
 
   test('登録解除', function () {
     /** 登録を解除する。 */
-    const unregister = registerDefaultProcedure('procedure', async function () {});
-    expect(isDefaultProcedureRegistered('procedure')).toBe(true);
+    const unregister = registerProcedure('procedure', async function () {});
+    expect(isProcedureRegistered('procedure')).toBe(true);
     unregister();
-    expect(isDefaultProcedureRegistered('procedure')).toBe(false);
+    expect(isProcedureRegistered('procedure')).toBe(false);
   });
+
+  test('上書き', function () {
+    /** 手続き名 */
+    const procedureName = 'procedure';
+    /** 手続き1 */
+    async function procedure1() {
+      return true;
+    }
+    /** 手続き2 */
+    async function procedure2() {
+      return true;
+    }
+    registerProcedure(procedureName, procedure1);
+    expect(isProcedureRegistered(procedureName)).toBe(true);
+    expect(isProcedureRegistered(procedureName, procedure1)).toBe(true);
+
+    registerProcedure(procedureName, procedure2);
+    expect(isProcedureRegistered(procedureName)).toBe(true);
+    expect(isProcedureRegistered(procedureName, procedure1)).toBe(false);
+    expect(isProcedureRegistered(procedureName, procedure2)).toBe(true);
+  });
+
+  test('既定の手続きの復元');
 });
