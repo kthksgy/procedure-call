e4fef52c96e66b375676fbda883dcc10e84bc002
kthksgy <1616191+kthksgy@users.noreply.github.com>
2023-08-05 17:58:33 +0900

    定義を追加

---
+++ packages/core/src/vite.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="vite/client" />
+
+/**
+ * バージョン(`package.json` `version`)
+ *
+ * @requires vite.config.ts
+ * ```
+ * export default defineConfig({
+ *   // ...
+ *   define: {
+ *     // ...
+ *     __version: JSON.stringify(process.env.npm_package_version),
+ *     // ...
+ *   },
+ *   // ...
+ * });
+ * ```
+ */
+declare const __version: string;
