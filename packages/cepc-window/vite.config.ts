c795c9746e62c1c883554a27fc361ecc00534eff
kthksgy <1616191+kthksgy@users.noreply.github.com>
2023-08-06 18:33:27 +0900

    `packages/cepc-window`を初期化

---
+++ packages/cepc-window/vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'path';
+
+import { defineConfig } from 'vite';
+
+export default defineConfig(function () {
+  return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        fileName: 'index',
+        name: 'cepc',
+      },
+      outDir: 'lib',
+      sourcemap: true,
+    },
+
+    define: {
+      /** バージョン(`package.json` `version`) */
+      __version: `'${process.env.npm_package_version}'`,
+      // `if(import.meta.vitest) { ... }`を削除する。
+      'import.meta.vitest': 'undefined',
+    },
+
+    test: {
+      includeSource: ['src/**/*.{js,ts}'],
+    },
+  };
+});
