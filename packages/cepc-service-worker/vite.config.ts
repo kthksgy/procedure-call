fa7ba2fa1f473cc6666d979f4b5a7e4a9fa34cd7
kthksgy <1616191+kthksgy@users.noreply.github.com>
2023-08-08 01:02:19 +0900

    `vite.config.ts`を調整

--- packages/cepc-service-worker/vite.config.ts
+++ packages/cepc-service-worker/vite.config.ts
@@ -1,32 +1,61 @@
+import { existsSync, readFileSync, statSync } from 'fs';
 import { resolve } from 'path';
 
 import { defineConfig } from 'vite';
 
-import { peerDependencies } from './package.json';
+import type { UserConfig as ViteConfiguration } from 'vite';
+import type { UserConfig as VitestConfiguration } from 'vitest';
+
+/** `UpperCamelCase`に変換する。 */
+function upperCamelCase(s: string) {
+  return s.replace(/(?:^|-|_)([^\-_]*)/g, function (_, part: string) {
+    return part.length > 0
+      ? part.charAt(0).toUpperCase() +
+          (/^[0-9A-Z]*$/.test(part) ? part.slice(1).toLowerCase() : part.slice(1))
+      : '';
+  });
+}
 
 export default defineConfig(function () {
-  return {
+  /** `package.json`のパス */
+  const libraryPath = resolve(__dirname, 'package.json');
+  /** `package.json` */
+  let library: { name: string; peerDependencies?: Partial<Record<string, string>> };
+  if (existsSync(libraryPath) && statSync(libraryPath).isFile()) {
+    library = JSON.parse(readFileSync(libraryPath, 'utf8'));
+    if (typeof library.name !== 'string' || library.name.length === 0) {
+      throw new Error('`name` is required.');
+    }
+    if (
+      (typeof library.peerDependencies !== 'object' || library.peerDependencies === null) &&
+      library.peerDependencies !== undefined
+    ) {
+      throw new Error('`peerDependencies` is invalid.');
+    }
+  } else {
+    throw new Error(`\`${libraryPath}\` does not exist.`);
+  }
+
+  /** 設定 */
+  const configuration: ViteConfiguration & VitestConfiguration = {
     build: {
       lib: {
-        entry: resolve(__dirname, 'src/index.ts'),
+        entry: resolve(__dirname, 'src', 'index.ts'),
         fileName: 'index',
-        name: 'index',
+        name: upperCamelCase(library.name),
       },
       outDir: 'lib',
       rollupOptions: {
-        external: [...Object.keys(peerDependencies)],
+        external: [...(library?.peerDependencies ? Object.keys(library.peerDependencies) : [])],
         output: {
           globals: {
-            ...Object.fromEntries(
-              Object.keys(peerDependencies).map(function (key) {
-                return [
-                  key,
-                  key.replace(/(?:^|-)(.)/g, function (_, character: string) {
-                    return character.toUpperCase();
+            ...(library?.peerDependencies
+              ? Object.fromEntries(
+                  Object.keys(library?.peerDependencies).map(function (key) {
+                    return [key, upperCamelCase(key)];
                   }),
-                ];
-              }),
-            ),
+                )
+              : {}),
           },
         },
       },
@@ -44,4 +73,8 @@ export default defineConfig(function () {
       includeSource: ['src/**/*.{js,ts}'],
     },
   };
+
+  console.debug(JSON.stringify(configuration, null, 2));
+
+  return configuration;
 });
