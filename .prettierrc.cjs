d7d916c3357aa5bdbcebf02589ddf5e0a6576074
kthksgy <1616191+kthksgy@users.noreply.github.com>
2023-08-09 13:42:10 +0900

    Prettify

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -14,25 +14,25 @@ $ npm i -D prettier@~3.0.1
 
 module.exports = {
   /** アロー関数の括弧の引数に括弧を付ける。 */
-  arrowParens: "always",
+  arrowParens: 'always',
   /** 複数行のHTML要素の閉じ括弧を最後の行の末尾に挿入する。 */
   bracketSameLine: false,
   /** オブジェクトリテラルの括弧の間にスペースを挿入する。 */
   bracketSpacing: true,
   /** 改行コード */
-  endOfLine: "lf",
+  endOfLine: 'lf',
   /** JSX内でダブルクォーテーションの代わりにシングルクォーテーションを使用する。 */
   jsxSingleQuote: false,
   /** 1行の最大文字数 */
   printWidth: 100,
   /** オブジェクトのプロパティ名をクォーテーションで囲む。 */
-  quoteProps: "as-needed",
+  quoteProps: 'as-needed',
   /** 文の最後にセミコロンを挿入する。 */
   semi: true,
   /** ダブルクォーテーションの代わりにシングルクォーテーションを使用する。 */
   singleQuote: true,
   /** 末尾のカンマのスタイル */
-  trailingComma: "all",
+  trailingComma: 'all',
   /** インデント1段階に対応する半角スペース数 */
   tabWidth: 2,
   /** インデントとして半角スペースの代わりにタブを使用する。 */
